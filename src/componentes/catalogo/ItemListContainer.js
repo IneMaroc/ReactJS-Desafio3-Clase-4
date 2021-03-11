8d49ac1baff310d0f81f05923ce953512925aa10
IneMaroc <ine.marocchi@gmail.com>
2021-03-10 22:11:01 -0300

    primer commit

---
+++ src/componentes/catalogo/ItemListContainer.js
@@ -0,0 +1,13 @@
+
+function ItemListContainer () {
+    return (
+
+    <h1 className="text-center"> Estamos armando nuestro catálogo, Vuelve Pronto! </h1>
+        
+       
+
+);
+}
+
+
+export default ItemListContainer;
---
+++ src/componentes/navbar/CartWidget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ReactComponent as Canasta} from './cestaCompra.svg';
+
+function CartWidget () {
+
+    return (
+        
+        <Canasta/>
+        
+    )
+}
+
+export default CartWidget;
